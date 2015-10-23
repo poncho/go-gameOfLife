59fb703264c5e71d33dccee97a163a3879baabdf
Poncho Mtz <poncho.811@gmail.com>
2015-10-23 02:46:05 -0500

    Living cells slice added (show function also) and testing startGame.

--- gameOfLife.go
+++ gameOfLife.go
@@ -5,16 +5,20 @@ import (
 )
 
 type World struct {
-	num int
-	m   [20][20]string
+	num         int
+	m           [20][20]string
+	livingCells []Cell
+}
+
+type Cell struct {
+	x int
+	y int
 }
 
 func (w *World) initLife() {
 	w.setCell(10, 11)
 	w.setCell(10, 12)
 	w.setCell(10, 13)
-	w.setCell(11, 11)
-	w.setCell(11, 12)
 
 	w.fillWorld()
 }
@@ -39,20 +43,39 @@ func (w *World) fillWorld() {
 	}
 }
 
+func (w World) printCells() {
+	for _, cell := range w.livingCells {
+		fmt.Println(cell)
+	}
+}
+
 func (w *World) setCell(x int, y int) {
 	w.m[x][y] = "*"
+	w.livingCells = append(w.livingCells, Cell{x, y})
 }
 
 func (w *World) killCell(x int, y int) {
 	w.m[x][y] = ""
 }
 
+func (w *World) startGame() {
+	endGame := false
+
+	for endGame == false {
+		fmt.Println("TEST")
+		endGame = true
+	}
+}
+
 func main() {
 	w := &World{
 		0,
 		[20][20]string{},
+		make([]Cell, 0),
 	}
 
 	w.initLife()
 	w.printWorld()
+	w.printCells()
+	w.startGame()
 }
