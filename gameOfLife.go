490ce0361efc30eb33517dfc70094d09e852a92c
Poncho <poncho@mail.com>
2015-10-23 17:42:43 -0500

    Checks added and clear console

--- gameOfLife.go
+++ gameOfLife.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 )
 
 type World struct {
@@ -24,6 +26,7 @@ func (w *World) initLife() {
 }
 
 func (w World) printWorld() {
+
 	for _, line := range w.m {
 		for _, cell := range line {
 			fmt.Print(cell)
@@ -49,6 +52,49 @@ func (w World) printCells() {
 	}
 }
 
+func (w *World) checkCell(c Cell) int {
+	livingNeighbors := 0
+
+	if c.x-1 >= 0 {
+		if w.m[c.x-1][c.y] == "*" {
+			livingNeighbors += 1
+		}
+	}
+
+	if c.x+1 <= len(w.m)-1 {
+		if w.m[c.x+1][c.y] == "*" {
+			livingNeighbors += 1
+		}
+	}
+
+	if c.y-1 >= 0 {
+		if w.m[c.x][c.y-1] == "*" {
+			livingNeighbors += 1
+		}
+	}
+
+	if c.y+1 <= len(w.m)-1 {
+		if w.m[c.x][c.y+1] == "*" {
+			livingNeighbors += 1
+		}
+	}
+	fmt.Println(c.x, ",", c.y, " : ", livingNeighbors)
+	return livingNeighbors
+}
+
+func (w *World) checkDeadCells() {
+
+	for i, _ := range w.m {
+		for j, _ := range w.m[i] {
+			if w.m[i][j] == " " {
+				if w.checkCell(Cell{i, j}) == 3 {
+					//w.setCell(i, j)
+				}
+			}
+		}
+	}
+}
+
 func (w *World) setCell(x int, y int) {
 	w.m[x][y] = "*"
 	w.livingCells = append(w.livingCells, Cell{x, y})
@@ -60,10 +106,28 @@ func (w *World) killCell(x int, y int) {
 
 func (w *World) startGame() {
 	endGame := false
+	var quit string
+	//var i int
 
 	for endGame == false {
-		fmt.Println("TEST")
-		endGame = true
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+		w.printWorld()
+
+		w.checkDeadCells()
+
+		/*
+			for _, cell := range w.livingCells {
+				_ = w.checkCell(cell)
+			}
+		*/
+
+		fmt.Scanf("%s", &quit)
+
+		if quit == "q" || quit == "Q" {
+			endGame = true
+		}
 	}
 }
 
@@ -75,7 +139,7 @@ func main() {
 	}
 
 	w.initLife()
-	w.printWorld()
-	w.printCells()
+	//w.printWorld()
+	//w.printCells()
 	w.startGame()
 }
